0d82792a1617a3ff746e82ee1eb0996b298fd92e
Flo <florensiagondokusumo@gmail.com>
2022-10-19 16:45:32 +0700

    fix: fix the migration model to enable proper reseeding
    If applied, this commit will allow proper migration and reseeding on  "test:db:reset:seed" run

---
+++ migrations/discount.js
@@ -0,0 +1,10 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return Promise.resolve();
+  },
+  async down(queryInterface, Sequelize) {
+    return Promise.resolve();
+  }
+};
--- migrations/product.js
+++ migrations/product.js
@@ -14,10 +14,42 @@ module.exports = {
       },
       unitPrice: {
         type: Sequelize.INTEGER
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+
+    await queryInterface.createTable('Discounts', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        foreignKey: true,
+        references: {
+          model: 'Products',
+          key: 'id'
+        }
+      },
+      minPurchase: {
+        type: Sequelize.INTEGER
+      },
+      discount: {
+        type: Sequelize.INTEGER
       }
     });
   },
   async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Discounts');
     await queryInterface.dropTable('Products');
   }
 };
