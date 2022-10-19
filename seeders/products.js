9577fc72e66f0080df8040b5600f2c98fc19042d
Flo <florensiagondokusumo@gmail.com>
2022-10-19 16:42:21 +0700

    test: introducing seeders module to populate db
    If applied, this commit will allow running seed command to populate the database with these data

--- seeders/products.js
+++ seeders/products.js
@@ -5,18 +5,43 @@ const { generateRandomString, generateRandomNumber } = require("../utils/random"
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    let seeds = [];
+    let seeds = [{
+      name: "Rolex",
+      unitPrice: 100,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    },{
+      name: "Michael Kors",
+      unitPrice: 80,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    },{
+      name: "Swatch",
+      unitPrice: 50,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    },{
+      name: "Casio",
+      unitPrice: 30,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    },
+  ];
 
-    for(let i = 0; i < testing.seedAmount; i++) {
-      const seedData = {
-        name: generateRandomString(5),
-        unitPrice: generateRandomNumber(),
-      }
+    return queryInterface.bulkInsert("Products", seeds).then(() => {
+      let seeds = [{
+        productId: 1,   
+        minPurchase: 3,
+        discount: 200,     
+      },
+      {
+        productId: 2,
+        minPurchase: 2,
+        discount: 120,       
+      }];
 
-      seeds.push(seedData);
-    }
-
-    return queryInterface.bulkInsert("Products", seeds);
+      return queryInterface.bulkInsert("Discounts", seeds);
+    });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("Products", null, {});
